b6c521266d02e50e162ffe05fad0e69b11e2eb61
Sidath Induwara <sidathgit@gmail.com>
2026-09-15 01:24:38 +0530

    Connect frontend to Render API

--- admin.js
+++ admin.js
@@ -8,8 +8,7 @@
    CONFIG
 ========================================================= */
 
-const API_URL =
-    "http://localhost:5000/api";
+const API_URL = "https://sahanya-store.onrender.com/api";
 
 
 /* =========================================================
--- script.js
+++ script.js
@@ -7,7 +7,7 @@
    API CONFIGURATION
 ========================================================= */
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://sahanya-store.onrender.com/api";
 
 
 /* =========================================================
